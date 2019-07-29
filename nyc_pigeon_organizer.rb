42ab903e191d539f945ba20ad0edf03a36cc4ba4
inherited-chain-1307 <lfrederick8@hotmail.com>
2019-07-29 18:38:53 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,5 +1,7 @@
+names = ["Theo", "Peter Jr.", "Lucky", "Ms. K", "Queenie", "Andrew", "Alex"]
+
 def nyc_pigeon_organizer(data)
-  names = ["Theo", "Peter Jr.", "Lucky", "Ms. K", "Queenie", "Andrew", "Alex"]
+  
   pigeon_list = Hash[names.collect { |item| [item, :color] } ]
   
 end
