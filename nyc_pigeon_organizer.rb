263f8d17ec82db033d2a7f7b068521b58f5c8399
inherited-chain-1307 <lfrederick8@hotmail.com>
2019-07-29 18:42:36 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,10 +1,5 @@
 names = ["Theo", "Peter Jr.", "Lucky", "Ms. K", "Queenie", "Andrew", "Alex"]
 
 def nyc_pigeon_organizer(data)
-  pigeon_data.each do |key, value|
-  puts key
-  value.each do |k,v|
-    puts k
-    puts v
-  end
+  pigeon_data.each do 
 end
